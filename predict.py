6a078c7fa0cbd1a6f1d39bc890343200477ccc4f
aliu-x1 <liuhan0518@gmail.com>
2018-04-27 00:47:25 +0800

    initial

---
+++ predict.py
@@ -0,0 +1,22 @@
+import sys
+import json
+import numpy as np
+import kenlm
+
+def predict(dump_file, model):
+	rank = {}
+	with open(dump_file) as f:
+		for line in f:
+			print line
+			pyjson = json.loads(line)
+			#print pyjson['position'] + " !!!!"
+			score = model.score(pyjson['sequence'])
+			rank[line] = score
+			print rank[line]
+
+def main(dump_file):
+	model = kenlm.LanguageModel('/home/aliu/Research/Projects/sgram/model/2018-4-26_hang.klm')
+	predict(dump_file, model)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
